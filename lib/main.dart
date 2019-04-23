6ba91a3ad3b064e418c2b52d7e713c65ef48284f
jwi <jan.wiesbauer@icloud.com>
2019-04-23 15:37:58 +0200

    Fix project structure

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SharePool',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: DriverPage(),
-    );
+        title: 'SharePool',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: DriverPage());
   }
 }
