e91a28b58d0ebc15c4b6d531e98603b4fed1128e
jwi <jan.wiesbauer@icloud.com>
2019-06-06 12:11:27 +0200

    Add UserLoginDto and allow login with email or username

--- lib/util/rest/UserRestClient.dart
+++ lib/util/rest/UserRestClient.dart
@@ -3,13 +3,14 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:share_pool/common/Constants.dart';
 import 'package:share_pool/model/dto/user/UserDto.dart';
+import 'package:share_pool/model/dto/user/UserLoginDto.dart';
 import 'package:share_pool/model/dto/user/UserTokenDto.dart';
 
 class UserRestClient {
   static const String BASE_URL = Constants.BASE_REST_URL + "/users";
 
-  static Future<UserCredentialsDto> loginUser(UserDto userDto) async {
-    var body = json.encode(userDto);
+  static Future<UserCredentialsDto> loginUser(UserLoginDto userLoginDto) async {
+    var body = json.encode(userLoginDto);
 
     var response = await post(BASE_URL,
         body: body, headers: {"Content-Type": "application/json"});
