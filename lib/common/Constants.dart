3706b6f02f34620e45b0146ae33ab72890aa8263
jwi <jan.wiesbauer@icloud.com>
2019-06-08 19:50:35 +0200

    Remove unused constant

--- lib/common/Constants.dart
+++ lib/common/Constants.dart
@@ -1,9 +1,7 @@
 class Constants {
-  static const String BASE_REST_URL = "http://192.168.0.7:8080";
+  static const String BASE_REST_URL = "http://192.168.0.100:8080";
 
   static const String SETTINGS_USER_TOKEN = "userToken";
   static const String SETTINGS_USER_ID = "userId";
   static const String SETTINGS_LOGGED_IN_USER = "loggedInUser";
-
-  static const String HTTP_AUTHORIZATION = "Authorization";
 }
