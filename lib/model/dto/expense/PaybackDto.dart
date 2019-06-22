4a5e3c92e65c6dbabdb607e10c00a1ffe1e654f3
Tobias Kaderle <kaderle.tobias@hotmail.com>
2019-06-22 10:20:16 +0200

    Finished main workflow

---
+++ lib/model/dto/expense/PaybackDto.dart
@@ -0,0 +1,6 @@
+import 'package:share_pool/model/dto/user/UserDto.dart';
+
+class PaybackDto {
+  UserDto receiver;
+  double amount;
+}
--- lib/statistics/singleUserExpensesPage.dart
+++ lib/statistics/singleUserExpensesPage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:share_pool/driver-settings/tourListWidget.dart';
 import 'package:share_pool/model/dto/expense/ExpensePerUserDto.dart';
+import 'package:share_pool/model/dto/expense/PaybackDto.dart';
+import 'package:share_pool/model/dto/user/UserDto.dart';
 import 'package:share_pool/mydrawer.dart';
 import 'package:share_pool/statistics/statistics_page.dart';
 
@@ -21,6 +23,15 @@ class SingleUserExpensesPage extends StatelessWidget {
         appBar: AppBar(
           title: Text(_title),
         ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          PaybackForm(myDrawer, _expensePerUserDto.userDto)));
+            }),
         drawer: myDrawer,
         body: Column(
           children: <Widget>[
@@ -50,5 +61,120 @@ class SingleUserExpensesPage extends StatelessWidget {
             )
           ],
         ));
+  }
+}
+
+class PaybackForm extends StatefulWidget {
+  UserDto userDto;
+
+  var myDrawer;
+
+  PaybackForm(this.myDrawer, this.userDto);
+
+  @override
+  _PaybackFormState createState() => _PaybackFormState();
+}
+
+class _PaybackFormState extends State<PaybackForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  PaybackDto paybackDto;
+
+  _PaybackFormState() {
+    this.paybackDto = new PaybackDto();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Payback with ${widget.userDto.userName}"),
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Amount you want to pay back",
+                      suffixIcon: Icon(Icons.attach_money)),
+                  keyboardType: TextInputType.numberWithOptions(
+                      signed: false, decimal: true),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Amount must be set";
+                    }
+
+                    if (double.parse(value) < 0.1) {
+                      return "Value must be over 0.1";
+                    }
+                  },
+                  onSaved: (String value) {
+                    paybackDto.amount = double.parse(value);
+                  },
+                  initialValue: null,
+                ),
+                Row(
+                  children: <Widget>[
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: RaisedButton(
+                        child: Text("Submit"),
+                        onPressed: () {
+                          var formState = _formKey.currentState;
+                          if (formState.validate()) {
+                            formState.save();
+
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text("Confirm payback"),
+                                    content: Text(
+                                        "Do you really want to pay back ${paybackDto
+                                            .amount.toStringAsFixed(
+                                            2)} to ${widget.userDto
+                                            .userName}?"),
+                                    actions: <Widget>[
+                                      new FlatButton(
+                                        child: new Text("No"),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                      new RaisedButton(
+                                        child: new Text("Yes"),
+                                        textColor: Colors.white,
+                                        onPressed: () =>
+                                            confirmPayback(paybackDto),
+                                      ),
+                                    ],
+                                  );
+                                });
+                          }
+                        },
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  confirmPayback(PaybackDto paybackDto) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) =>
+                StatisticsPage(widget.myDrawer)));
   }
 }
