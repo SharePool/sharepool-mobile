5b897105290c71262595dae91d3025c19d69b3ce
jwi <jan.wiesbauer@icloud.com>
2019-06-27 21:30:10 +0200

    Fix typo in url

--- lib/common/Constants.dart
+++ lib/common/Constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   static const String BASE_SERVER_REST_URL = "http://geanik.ddns.net:8080";
-  static const String BASE_ANALYTICS_REST_URL = "http://geanik.ddns.nets:8081";
+  static const String BASE_ANALYTICS_REST_URL = "http://geanik.ddns.net:8081";
 
   static const String SETTINGS_USER_TOKEN = "userToken";
   static const String SETTINGS_USER_ID = "userId";
